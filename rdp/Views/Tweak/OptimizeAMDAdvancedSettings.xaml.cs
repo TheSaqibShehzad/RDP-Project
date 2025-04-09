e58fe3b2864913e926f9d47f131107f2006d6bc3
KhadimHussainDev <oyeKhadim@gmail.com>
2025-04-09 21:02:39 +0500

    all twaeks gui completed

---
+++ rdp/Views/Tweak/OptimizeAMDAdvancedSettings.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace rdp.Views.Tweak
+{
+    /// <summary>
+    /// Interaction logic for UCMitigations.xaml
+    /// </summary>
+    public partial class OptimizeAMDAdvancedSettings : UserControl
+    {
+        public OptimizeAMDAdvancedSettings()
+        {
+            InitializeComponent();
+        }
+        public void Toggle_Checked(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Checked");
+            //Tweaks.ApiTest("", true , false );
+        }
+        public void Toggle_UnChecked(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Unchecked");
+        }
+    }
+}
