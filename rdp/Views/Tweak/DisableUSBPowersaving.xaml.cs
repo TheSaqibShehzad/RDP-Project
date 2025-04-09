f934f5493c420c1f919131c473775a9daca253f3
KhadimHussainDev <oyeKhadim@gmail.com>
2025-04-09 17:31:07 +0500

    disable windows tips tweak added

---
+++ rdp/Views/Tweak/DisableUSBPowersaving.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace rdp.Views.Tweak
+{
+    /// <summary>
+    /// Interaction logic for UCMitigations.xaml
+    /// </summary>
+    public partial class DisableUSBPowersaving : UserControl
+    {
+        public DisableUSBPowersaving()
+        {
+            InitializeComponent();
+        }
+        public void Toggle_Checked(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Checked");
+        }
+    }
+}
---
+++ rdp/Views/Tweak/DisableWindowsInsiderExperiments.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace rdp.Views.Tweak
+{
+    /// <summary>
+    /// Interaction logic for UCMitigations.xaml
+    /// </summary>
+    public partial class DisableWindowsInsiderExperiments : UserControl
+    {
+        public DisableWindowsInsiderExperiments()
+        {
+            InitializeComponent();
+        }
+        public void Toggle_Checked(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Checked");
+        }
+    }
+}
